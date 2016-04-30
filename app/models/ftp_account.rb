1e92aacc877d960f926a5e27c5d0cb86cb24fe6c
koshigoe <koshigoeb@gmail.com>
2016-04-30 20:26:06 +0900

    $ rake annotate_models

--- app/models/ftp_account.rb
+++ app/models/ftp_account.rb
@@ -1,2 +1,21 @@
+# ## Schema Information
+#
+# Table name: `ftp_accounts`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`id`**          | `integer`          | `not null, primary key`
+# **`username`**    | `string`           | `not null`
+# **`password`**    | `string`           | `not null`
+# **`uid`**         | `integer`          | `default("500"), not null`
+# **`gid`**         | `integer`          | `default("500"), not null`
+# **`homedir`**     | `string`           | `default("/home/vagrant"), not null`
+# **`shell`**       | `string`           | `default(""), not null`
+# **`created_at`**  | `datetime`         | `not null`
+# **`updated_at`**  | `datetime`         | `not null`
+#
+
 class FtpAccount < ApplicationRecord
 end
