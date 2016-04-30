9df7fd76d3751573c6dc015cc32f314f73ef478d
koshigoe <koshigoeb@gmail.com>
2016-05-01 00:07:07 +0900

    Change column default value.

--- spec/factories/ftp_accounts.rb
+++ spec/factories/ftp_accounts.rb
@@ -9,9 +9,9 @@
 # **`id`**               | `integer`          | `not null, primary key`
 # **`username`**         | `string`           | `not null`
 # **`password_digest`**  | `string`           | `not null`
-# **`uid`**              | `integer`          | `default("500"), not null`
-# **`gid`**              | `integer`          | `default("500"), not null`
-# **`homedir`**          | `string`           | `default("/home/vagrant"), not null`
+# **`uid`**              | `integer`          | `not null`
+# **`gid`**              | `integer`          | `not null`
+# **`homedir`**          | `string`           | `not null`
 # **`shell`**            | `string`           | `default(""), not null`
 # **`created_at`**       | `datetime`         | `not null`
 # **`updated_at`**       | `datetime`         | `not null`
@@ -21,5 +21,9 @@ FactoryGirl.define do
   factory :ftp_account do
     sequence(:username) { |n| "ftp-#{n}" }
     password "{sha256}#{Base64.strict_encode64 Digest::SHA256.digest('password')}"
+    uid 1000
+    gid 1000
+    homedir '/home/vagrant'
+    shell ''
   end
 end
--- spec/models/ftp_account_spec.rb
+++ spec/models/ftp_account_spec.rb
@@ -9,9 +9,9 @@
 # **`id`**               | `integer`          | `not null, primary key`
 # **`username`**         | `string`           | `not null`
 # **`password_digest`**  | `string`           | `not null`
-# **`uid`**              | `integer`          | `default("500"), not null`
-# **`gid`**              | `integer`          | `default("500"), not null`
-# **`homedir`**          | `string`           | `default("/home/vagrant"), not null`
+# **`uid`**              | `integer`          | `not null`
+# **`gid`**              | `integer`          | `not null`
+# **`homedir`**          | `string`           | `not null`
 # **`shell`**            | `string`           | `default(""), not null`
 # **`created_at`**       | `datetime`         | `not null`
 # **`updated_at`**       | `datetime`         | `not null`
