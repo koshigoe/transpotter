8b2dacfa551b8fe762d97d2ecb7156954251a1ff
koshigoe <koshigoeb@gmail.com>
2016-04-30 22:46:05 +0900

    Specify API version via URL path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope 'v1.0' do
+  scope 'v:version', constraints: { version: /\d+\.\d+/ } do
     resources :authentications, only: [:create]
   end
 end
