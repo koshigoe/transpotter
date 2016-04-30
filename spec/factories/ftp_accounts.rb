b37423e26f012492bb5961054a9c4c25b383fb42
koshigoe <koshigoeb@gmail.com>
2016-04-30 23:42:14 +0900

    Rename password password_digest

--- spec/factories/ftp_accounts.rb
+++ spec/factories/ftp_accounts.rb
@@ -4,21 +4,22 @@
 #
 # ### Columns
 #
-# Name              | Type               | Attributes
-# ----------------- | ------------------ | ---------------------------
-# **`id`**          | `integer`          | `not null, primary key`
-# **`username`**    | `string`           | `not null`
-# **`password`**    | `string`           | `not null`
-# **`uid`**         | `integer`          | `default("500"), not null`
-# **`gid`**         | `integer`          | `default("500"), not null`
-# **`homedir`**     | `string`           | `default("/home/vagrant"), not null`
-# **`shell`**       | `string`           | `default(""), not null`
-# **`created_at`**  | `datetime`         | `not null`
-# **`updated_at`**  | `datetime`         | `not null`
+# Name                   | Type               | Attributes
+# ---------------------- | ------------------ | ---------------------------
+# **`id`**               | `integer`          | `not null, primary key`
+# **`username`**         | `string`           | `not null`
+# **`password_digest`**  | `string`           | `not null`
+# **`uid`**              | `integer`          | `default("500"), not null`
+# **`gid`**              | `integer`          | `default("500"), not null`
+# **`homedir`**          | `string`           | `default("/home/vagrant"), not null`
+# **`shell`**            | `string`           | `default(""), not null`
+# **`created_at`**       | `datetime`         | `not null`
+# **`updated_at`**       | `datetime`         | `not null`
 #
 
 FactoryGirl.define do
   factory :ftp_account do
-    
+    sequence(:username) { |n| "ftp-#{n}" }
+    password "{sha256}#{Base64.strict_encode64 Digest::SHA256.digest('password')}"
   end
 end
--- spec/models/ftp_account_spec.rb
+++ spec/models/ftp_account_spec.rb
@@ -4,17 +4,17 @@
 #
 # ### Columns
 #
-# Name              | Type               | Attributes
-# ----------------- | ------------------ | ---------------------------
-# **`id`**          | `integer`          | `not null, primary key`
-# **`username`**    | `string`           | `not null`
-# **`password`**    | `string`           | `not null`
-# **`uid`**         | `integer`          | `default("500"), not null`
-# **`gid`**         | `integer`          | `default("500"), not null`
-# **`homedir`**     | `string`           | `default("/home/vagrant"), not null`
-# **`shell`**       | `string`           | `default(""), not null`
-# **`created_at`**  | `datetime`         | `not null`
-# **`updated_at`**  | `datetime`         | `not null`
+# Name                   | Type               | Attributes
+# ---------------------- | ------------------ | ---------------------------
+# **`id`**               | `integer`          | `not null, primary key`
+# **`username`**         | `string`           | `not null`
+# **`password_digest`**  | `string`           | `not null`
+# **`uid`**              | `integer`          | `default("500"), not null`
+# **`gid`**              | `integer`          | `default("500"), not null`
+# **`homedir`**          | `string`           | `default("/home/vagrant"), not null`
+# **`shell`**            | `string`           | `default(""), not null`
+# **`created_at`**       | `datetime`         | `not null`
+# **`updated_at`**       | `datetime`         | `not null`
 #
 
 require 'rails_helper'
