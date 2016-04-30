c54f5529aef07c65a74a3123b88aaba96bdd9dd7
koshigoe <koshigoeb@gmail.com>
2016-04-30 20:26:33 +0900

    Create `users` table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
+# ## Schema Information
+#
+# Table name: `users`
+#
+# ### Columns
+#
+# Name                   | Type               | Attributes
+# ---------------------- | ------------------ | ---------------------------
+# **`id`**               | `integer`          | `not null, primary key`
+# **`username`**         | `string`           | `not null`
+# **`password_digest`**  | `string`           | `not null`
+#
+# ### Indexes
+#
+# * `idx_users_name` (_unique_):
+#     * **`username`**
+#
+
 class User < ApplicationRecord
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,21 @@
+# ## Schema Information
+#
+# Table name: `users`
+#
+# ### Columns
+#
+# Name                   | Type               | Attributes
+# ---------------------- | ------------------ | ---------------------------
+# **`id`**               | `integer`          | `not null, primary key`
+# **`username`**         | `string`           | `not null`
+# **`password_digest`**  | `string`           | `not null`
+#
+# ### Indexes
+#
+# * `idx_users_name` (_unique_):
+#     * **`username`**
+#
+
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
