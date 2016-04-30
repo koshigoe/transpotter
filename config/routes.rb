01c68c06398685fe53d7bc276abba3bc2a5db1a2
koshigoe <koshigoeb@gmail.com>
2016-04-30 19:42:45 +0900

    Add route to authentications

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope 'v1.0' do
+    resources :authentications, only: [:create]
+  end
 end
