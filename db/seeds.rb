245654db492385f0b3eb26205faceafbb0a63582
koshigoe <koshigoeb@gmail.com>
2016-04-30 19:17:36 +0900

    Change SQLAuthTypes to OpenSSL (SHA256)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-FtpAccount.create_with(password: 'password').find_or_create_by!(username: 'ftp-1')
+require 'digest/sha2'
+
+FtpAccount.create_with(password: "{sha256}#{Base64.strict_encode64 Digest::SHA256.digest('password')}").find_or_create_by!(username: 'ftp-1')
