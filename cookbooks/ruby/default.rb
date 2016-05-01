a6243fab4c54c249bf345ae60de5a7904f99f02e
koshigoe <koshigoeb@gmail.com>
2016-05-01 16:37:43 +0900

    Install ruby to api host.

---
+++ cookbooks/ruby/default.rb
@@ -0,0 +1,7 @@
+package 'https://github.com/feedforce/ruby-rpm/releases/download/2.3.1/ruby-2.3.1-1.el7.centos.x86_64.rpm' do
+  not_if 'rpm -q ruby'
+end
+
+execute 'gem install bundler -v1.12.1 --no-document' do
+  not_if 'gem list -i ^bundler$ -v1.12.1'
+end
