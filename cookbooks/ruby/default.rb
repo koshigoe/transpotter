9d3410c231bd6c563f4de7309886966c8c5e5c69
koshigoe <koshigoeb@gmail.com>
2016-05-01 17:51:10 +0900

    Add packages to install gems

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,3 +1,5 @@
+include_recipe '../postgresql/devel.rb'
+
 package 'https://github.com/feedforce/ruby-rpm/releases/download/2.3.1/ruby-2.3.1-1.el7.centos.x86_64.rpm' do
   not_if 'rpm -q ruby'
 end
@@ -5,3 +7,8 @@ end
 execute 'gem install bundler -v1.12.1 --no-document' do
   not_if 'gem list -i ^bundler$ -v1.12.1'
 end
+
+package 'gcc'
+package 'postgresql95-devel'
+package 'libxml2-devel'
+package 'libxslt-devel'
--- serverspec/shared/ruby.rb
+++ serverspec/shared/ruby.rb
@@ -6,4 +6,20 @@ shared_examples_for 'Ruby' do
   describe package 'bundler' do
     it { should be_installed.by 'gem' }
   end
+
+  describe package 'gcc' do
+    it { should be_installed }
+  end
+
+  describe package 'postgresql95-devel' do
+    it { should be_installed }
+  end
+
+  describe package 'libxml2-devel' do
+    it { should be_installed }
+  end
+
+  describe package 'libxslt-devel' do
+    it { should be_installed }
+  end
 end
