eaa8f0b5d768c91d923bb25dcff499dd1dbd244a
koshigoe <koshigoeb@gmail.com>
2016-05-01 17:52:42 +0900

    Set linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,7 @@ set :repo_url, 'https://github.com/koshigoe/transpotter.git'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
