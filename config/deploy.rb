e8511a9f6af85e1ad5c278069b94abd86490dccf
koshigoe <koshigoeb@gmail.com>
2016-05-01 17:52:26 +0900

    Set application

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
 set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'app'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
