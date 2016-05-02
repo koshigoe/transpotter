e9e9d19f30e31464896370e6c6ac22823f9b24fd
koshigoe <koshigoeb@gmail.com>
2016-05-03 02:04:11 +0900

    $ rake annotate_models

--- app/models/sftp_account.rb
+++ app/models/sftp_account.rb
@@ -1,2 +1,21 @@
+# ## Schema Information
+#
+# Table name: `sftp_accounts`
+#
+# ### Columns
+#
+# Name                   | Type               | Attributes
+# ---------------------- | ------------------ | ---------------------------
+# **`id`**               | `integer`          | `not null, primary key`
+# **`username`**         | `string`           |
+# **`password_digest`**  | `string`           | `not null`
+# **`uid`**              | `integer`          | `not null`
+# **`gid`**              | `integer`          | `not null`
+# **`homedir`**          | `string`           | `not null`
+# **`shell`**            | `string`           | `default(""), not null`
+# **`created_at`**       | `datetime`         | `not null`
+# **`updated_at`**       | `datetime`         | `not null`
+#
+
 class SFTPAccount < ApplicationRecord
 end
--- spec/factories/sftp_accounts.rb
+++ spec/factories/sftp_accounts.rb
@@ -1,3 +1,22 @@
+# ## Schema Information
+#
+# Table name: `sftp_accounts`
+#
+# ### Columns
+#
+# Name                   | Type               | Attributes
+# ---------------------- | ------------------ | ---------------------------
+# **`id`**               | `integer`          | `not null, primary key`
+# **`username`**         | `string`           |
+# **`password_digest`**  | `string`           | `not null`
+# **`uid`**              | `integer`          | `not null`
+# **`gid`**              | `integer`          | `not null`
+# **`homedir`**          | `string`           | `not null`
+# **`shell`**            | `string`           | `default(""), not null`
+# **`created_at`**       | `datetime`         | `not null`
+# **`updated_at`**       | `datetime`         | `not null`
+#
+
 FactoryGirl.define do
   factory :sftp_account do
     
