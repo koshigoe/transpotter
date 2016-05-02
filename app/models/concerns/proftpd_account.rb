e66493219ad15a1a5a90a9e27e44b4c9d3ff5dd9
koshigoe <koshigoeb@gmail.com>
2016-05-03 02:12:15 +0900

    Share implementation

---
+++ app/models/concerns/proftpd_account.rb
@@ -0,0 +1,28 @@
+require 'digest/sha2'
+require 'base64'
+
+# proftpd_account
+module ProFTPDAccount
+  extend ActiveSupport::Concern
+
+  included do
+    validates :password, presence: true, on: :create
+    validates :uid, presence: true, numericality: { allow_blank: true }
+    validates :gid, presence: true, numericality: { allow_blank: true }
+    validates :homedir, presence: true
+
+    attr_readonly :username
+    attr_accessor :password
+
+    before_save do
+      self.password_digest = "{sha256}#{Base64.strict_encode64(Digest::SHA256.digest(password))}" if password.present?
+    end
+
+    after_create do
+      scheme = self.class.name.underscore.split('_').first
+      self.class.where(id: id).update_all("username = CONCAT('#{scheme}-', id)")
+      self.username = "#{scheme}-#{id}"
+      clear_attribute_changes([:username])
+    end
+  end
+end
--- app/models/ftp_account.rb
+++ app/models/ftp_account.rb
@@ -17,25 +17,6 @@
 # **`updated_at`**       | `datetime`         | `not null`
 #
 
-require 'digest/sha2'
-require 'base64'
-
 class FTPAccount < ApplicationRecord
-  validates :password, presence: true, on: :create
-  validates :uid, presence: true, numericality: { allow_blank: true }
-  validates :gid, presence: true, numericality: { allow_blank: true }
-  validates :homedir, presence: true
-
-  attr_readonly :username
-  attr_accessor :password
-
-  before_save do
-    self.password_digest = "{sha256}#{Base64.strict_encode64(Digest::SHA256.digest(password))}" if password.present?
-  end
-
-  after_create do
-    self.class.where(id: id).update_all('username = CONCAT(\'ftp-\', id)')
-    self.username = "ftp-#{id}"
-    clear_attribute_changes([:username])
-  end
+  include ProFTPDAccount
 end
