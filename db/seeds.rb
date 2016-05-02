baf96ac1e66502c576ed51f5081f2223de546b50
koshigoe <koshigoeb@gmail.com>
2016-05-03 03:12:21 +0900

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 FTPAccount.create_with(password: 'password', uid: 1000, gid: 1000, homedir: '/home/vagrant', shell: '').find_or_create_by!(username: 'ftp-1')
+SFTPAccount.create_with(password: 'password', uid: 1000, gid: 1000, homedir: '/home/vagrant', shell: '').find_or_create_by!(username: 'sftp-1')
 
 User.create_with(password: 'password', password_confirmation: 'password').find_or_create_by!(username: 'user1')
