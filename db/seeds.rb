c1b9a099b61d466b9d7034711cbf8c3e9f49235e
koshigoe <koshigoeb@gmail.com>
2016-05-03 18:52:57 +0900

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-FTPAccount.create_with(password: 'password', uid: 1000, gid: 1000, homedir: '/home/vagrant', shell: '').find_or_create_by!(username: 'ftp-1')
-SFTPAccount.create_with(password: 'password', uid: 1000, gid: 1000, homedir: '/home/vagrant', shell: '').find_or_create_by!(username: 'sftp-1')
+FTPAccount.create_with(password: 'password').find_or_create_by!(username: 'ftp-1')
+SFTPAccount.create_with(password: 'password').find_or_create_by!(username: 'sftp-1')
 
 User.create_with(password: 'password', password_confirmation: 'password').find_or_create_by!(username: 'user1')
