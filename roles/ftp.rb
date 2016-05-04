286ed129fa5df6a9311665aa97ed47330ec697fc
koshigoe <koshigoeb@gmail.com>
2016-05-04 20:41:45 +0900

    Fix order

--- roles/ftp.rb
+++ roles/ftp.rb
@@ -2,9 +2,9 @@ require 'itamae/secrets'
 node[:secrets] = Itamae::Secrets(File.join(__dir__, "../.secrets/#{node[:stage]}"))
 
 include_recipe '../cookbooks/environment/default.rb'
+include_recipe '../cookbooks/resolv/default.rb'
 include_recipe '../cookbooks/proftpd/default.rb'
 include_recipe '../cookbooks/git/default.rb'
 include_recipe '../cookbooks/ruby/default.rb'
 include_recipe '../cookbooks/pip/default.rb'
 include_recipe '../cookbooks/aws-cli/default.rb'
-include_recipe '../cookbooks/resolv/default.rb'
