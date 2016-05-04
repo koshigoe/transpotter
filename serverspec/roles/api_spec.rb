697ccdf6c45f13f39683725dafc53ff4632eaa85
koshigoe <koshigoeb@gmail.com>
2016-05-04 20:12:34 +0900

    Update resolv.conf at api server.

--- serverspec/roles/api_spec.rb
+++ serverspec/roles/api_spec.rb
@@ -3,4 +3,9 @@ require_relative '../spec_helper'
 RSpec.describe 'api role', type: :serverspec do
   it_behaves_like 'Git'
   it_behaves_like 'Ruby'
+  it_behaves_like 'resolv' do
+    describe file '/etc/resolv.conf' do
+      its(:content) { should match '^nameserver 192.168.33.53$' }
+    end
+  end
 end
