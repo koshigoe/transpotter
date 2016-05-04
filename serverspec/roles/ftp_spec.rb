1bf3858765f0e9d768ab2174bcd2640e881427c0
koshigoe <koshigoeb@gmail.com>
2016-05-04 20:17:15 +0900

    Update resolv.conf at ftp server.

--- serverspec/roles/ftp_spec.rb
+++ serverspec/roles/ftp_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe 'ftp role', type: :serverspec do
   it_behaves_like 'ProFTPD'
   it_behaves_like 'Git'
   it_behaves_like 'Ruby'
+  it_behaves_like 'resolv' do
+    describe file '/etc/resolv.conf' do
+      its(:content) { should match '^nameserver 192.168.33.53$' }
+    end
+  end
 end
